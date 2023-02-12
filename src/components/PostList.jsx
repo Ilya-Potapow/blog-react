4933a8343da3970a83bfc3cd4ecda2b874b2bbb7
Ilya Potapov <potapotapow.ilay22@gmail.com>
2023-02-12 13:40:47 +0200

    Add animations

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, title, remove }) => {
@@ -8,9 +9,18 @@ const PostList = ({ posts, title, remove }) => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((p, i) => (
-        <PostItem remove={remove} numb={i + 1} post={p} key={p.id} />
+      <TransitionGroup>
+        {posts.map((p, i) => (
+          <CSSTransition
+              key={p.id}
+              classNames='post'
+              timeout={500}
+          >
+            <PostItem remove={remove} numb={i + 1} post={p} />
+          </CSSTransition>
       ))}
+      </TransitionGroup>
+      
     </div>
   );
 };
